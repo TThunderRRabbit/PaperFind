44259e0c7808fc0fef410206081181c249daf7c3
ThunderRabbit <zuzazibubabi@gmail.com>
2025-01-06 03:31:18 +0800

    added a local storage and then displayed the title of the books

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [userInput, setUserInput] = useState("");
   const [view, setView] = useState("books");
+  const [bookCollection, setBookCollection] = useState(() => {
+    const savedBooks = localStorage.getItem("bookCollection");
+    return savedBooks ? JSON.parse(savedBooks) : [];
+  });
+
   const handleInputChange = (e) => {
     setUserInput(e.target.value);
   };
 
+  useEffect(() => {
+    localStorage.setItem("bookCollection", JSON.stringify(bookCollection)),
+      [bookCollection];
+  });
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(userInput);
-
     try {
       const response = await fetch(
         `https://openlibrary.org/search.json?title=${userInput}&limit=16`
@@ -19,6 +28,14 @@ function App() {
 
       const data = await response.json();
       console.log(data.docs);
+
+      const newBooks = data.docs.map((book) => {
+        return {
+          title: book.title,
+        };
+      });
+
+      setBookCollection(newBooks);
     } catch {
       console.error(error);
     }
@@ -33,7 +50,19 @@ function App() {
         </form>
       </div>
 
-      <div className="main">{view === "books" && <>Hello</>}</div>
+      <div className="main">
+        {view === "books" && (
+          <>
+            <ul>
+              {bookCollection.map((book, index) => (
+                <li key={index}>{book.title}</li>
+              ))}
+            </ul>
+          </>
+        )}
+
+        {view === "recommend" && <>s</>}
+      </div>
     </>
   );
 }
