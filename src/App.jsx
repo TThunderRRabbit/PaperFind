728add66de44527dec98fcd0bf6b51dfef4550db
ThunderRabbit <zuzazibubabi@gmail.com>
2025-01-06 14:25:40 +0800

    started working on the recommendation function

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import "./App.css";
 
 function App() {
   const [userInput, setUserInput] = useState("");
-  const [view, setView] = useState("books");
+  const [view, setView] = useState("recommend");
   const [bookCollection, setBookCollection] = useState(() => {
     const savedBooks = localStorage.getItem("bookCollection");
     return savedBooks ? JSON.parse(savedBooks) : [];
   });
+  const [recommendationCollection, setRecommendationCollection] = useState([]);
 
   const handleInputChange = (e) => {
     setUserInput(e.target.value);
   };
 
   useEffect(() => {
-    localStorage.setItem("bookCollection", JSON.stringify(bookCollection)),
-      [bookCollection];
-  });
+    localStorage.setItem("bookCollection", JSON.stringify(bookCollection));
+  }, [bookCollection]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -41,6 +41,35 @@ function App() {
     }
   };
 
+  const addToRecommendation = (genre) => {
+    setRecommendationCollection((prevGenre) => {
+      return [...prevGenre, genre];
+    });
+  };
+
+  const handleRecommendationSubmit = async (e) => {
+    e.preventDefault();
+    console.log(recommendationCollection);
+
+    try {
+      const response = recommendationCollection.map((genre) =>
+        fetch(`https://openlibrary.org/search.json?subject=${genre}&limit=10`)
+      );
+      const data = await Promise.all(response);
+      const recommendations = await Promise.all(
+        data.map((array) => array.json())
+      );
+
+      const newBooks = recommendations.flatMap((books) =>
+        books.docs ? books.docs.map((book) => ({ title: book.title })) : []
+      );
+
+      setBookCollection(newBooks);
+      setView("books");
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
       <div className="searchDiv">
@@ -61,7 +90,39 @@ function App() {
           </>
         )}
 
-        {view === "recommend" && <>s</>}
+        {view === "recommend" && (
+          <>
+            <form onSubmit={handleRecommendationSubmit}>
+              <div className="subjectsCollection">
+                <button
+                  type="button"
+                  onClick={() => addToRecommendation("Fiction")}
+                >
+                  Fiction
+                </button>
+                <button
+                  type="button"
+                  onClick={() => addToRecommendation("Children's fiction")}
+                >
+                  Children's
+                </button>
+                <button type="button">English Literature</button>
+                <button type="button">General</button>
+                <button type="button">History</button>
+                <button type="button">Juvenile</button>
+                <button type="button">Drama</button>
+                <button type="button">Criticism and interpretation</button>
+                <button type="button">Mystery & detective, general</button>
+                <button type="button">Fantasy</button>
+                <button type="button">Novela</button>
+                <button type="button">Adventure and adventurers</button>
+                <button type="button">Action & adventure</button>
+                <button type="button">Psychological</button>
+              </div>
+              <button type="submit">Submit</button>
+            </form>
+          </>
+        )}
       </div>
     </>
   );
